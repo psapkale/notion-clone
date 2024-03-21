6fb2e2a4bdb83ac5aac4638ee93b9204c1f3486f
Prem <prempravinsapkale@gmail.com>
2024-03-21 22:37:56 +0530

    auth setup

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 export const Navbar = () => {
+   const { isAuthenticated, isLoading } = useConvexAuth();
    const scrolled = useScrollTop();
 
    return (
@@ -17,6 +23,27 @@ export const Navbar = () => {
       >
          <Logo />
          <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+            {isLoading && <p>{<Spinner />}</p>}
+            {!isAuthenticated && !isLoading && (
+               <>
+                  <SignInButton mode="modal">
+                     <Button variant="ghost" size="sm">
+                        Login
+                     </Button>
+                  </SignInButton>
+                  <SignInButton mode="modal">
+                     <Button size="sm">Get Conception free</Button>
+                  </SignInButton>
+               </>
+            )}
+            {isAuthenticated && !isLoading && (
+               <>
+                  <Button variant="ghost" size="sm">
+                     <Link href="/documents">Enter Conception</Link>
+                  </Button>
+                  <UserButton afterSignOutUrl="/" />
+               </>
+            )}
             <ModeToggle />
          </div>
       </div>
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+   providers: [
+      {
+         domain: "https://polished-goose-48.clerk.accounts.dev",
+         applicationID: "convex",
+      },
+   ],
+};
