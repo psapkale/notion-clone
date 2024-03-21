8c4c0d6a266f4f32e1914ab946bdaf4d931833ea
Prem <prempravinsapkale@gmail.com>
2024-03-21 10:14:00 +0530

    dark mode in marketing page

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,6 +1,8 @@
+import { Navbar } from "./_components/navbar";
+
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
    return (
-      <div className="h-full">
+      <div className="h-full dark:bg-[#0f0f0f]">
          <Navbar />
          <main className="h-full pt-40">{children}</main>
       </div>
