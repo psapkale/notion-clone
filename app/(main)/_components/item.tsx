347979bfb17813a7ef3dffd6bb95e4f103943877
Prem <prempravinsapkale@gmail.com>
2024-03-26 20:38:47 +0530

    sidebar documents

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Skeleton } from "@/components/ui/skeleton";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
 import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
@@ -29,6 +30,13 @@ export const Item = ({
    onExpand,
    expanded,
 }: ItemProps) => {
+   const handleExpand = (
+      event: React.MouseEvent<HTMLDivElement, MouseEvent>
+   ) => {
+      event.stopPropagation();
+      onExpand?.();
+   };
+
    const ChevronIcon = expanded ? ChevronDown : ChevronRight;
 
    return (
@@ -47,7 +55,7 @@ export const Item = ({
             <div
                role="button"
                className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
-               onClick={() => {}}
+               onClick={handleExpand}
             >
                <ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground/50" />
             </div>
@@ -63,6 +71,24 @@ export const Item = ({
                <span className="text-xs">⌘</span>K
             </kbd>
          )}
+      </div>
+   );
+};
+
+Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
+   return (
+      <div
+         style={{
+            paddingLeft: level ? `${level * 12 + 25}px` : "12px",
+         }}
+         className="flex gap-x-2 py-[3px]"
+      >
+         {/*
+            Todo 
+            npx shadcn-ui@latest add skeleton 
+         */}
+         <Skeleton className="h-4 w-4" />
+         <Skeleton className="h-4 w-[30%]" />
       </div>
    );
 };
--- convex/documents.ts
+++ convex/documents.ts
@@ -4,15 +4,28 @@ import { v } from "convex/values";
 
 // api endpoints
 
-export const get = query({
-   handler: async (ctx) => {
+export const getSidebar = query({
+   args: {
+      parentDocument: v.optional(v.id("documents")),
+   },
+   handler: async (ctx, args) => {
       const identity = await ctx.auth.getUserIdentity();
 
       if (!identity) {
          throw new Error("Not authenticated");
       }
 
-      const documents = await ctx.db.query("documents").collect();
+      const userId = identity.subject;
+
+      // fetch parent documents
+      const documents = await ctx.db
+         .query("documents")
+         .withIndex("by_user_parent", (q) =>
+            q.eq("userId", userId).eq("parentDocument", args.parentDocument)
+         )
+         .filter((q) => q.eq(q.field("isArchived"), false))
+         .order("desc")
+         .collect();
 
       return documents;
    },
